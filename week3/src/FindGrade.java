f8a5f988c2baef27871a87b24a189bde6271ca63
YusufNevsi <106774776+YusufNevsi@users.noreply.github.com>
2025-03-07 12:16:08 +0300

    Add files via upload

--- week3/src/FindGrade.java
+++ week3/src/FindGrade.java
@@ -1,6 +1,29 @@
 public class FindGrade{
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+
+
+    int score = Integer.parseInt(args[0]);
+
+        if (score >= 90){
+        System.out.println("Grade: A");
+
+    }else if (score >= 80 && score <=89 ){
+        System.out.println("grade :B");
+
+    }else if (score >= 70 && score <=79 ){
+        System.out.println("grade :C");
+
+    }else if (score >= 60 && score <=69 ){
+        System.out.println("grade :D");
+
+    }else {
+            System.out.println("Grade :F");
+        }
+    }
+}
+
 
         //Get the score from the user as a file parameter
         //Calculate the grade based on the score
@@ -15,8 +38,7 @@ public class FindGrade{
 
 
 
-    }
 
 
 
-}
+
